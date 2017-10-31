955d1e4cde7c244aea9b8ef6a07f076c16bf4824
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2017-10-31 17:12:37 +0100

    Make a beter CLI with argparse, add better commends and timing.

--- whitening.py
+++ whitening.py
@@ -1,10 +1,43 @@
+"""
+Tries to separate text/line foreground and background by 2D median filter
+whitening.
+
+Example usage:
+
+Python API:
+
+```
+from PIL import Image
+from whitening import whiten
+
+image = np.asarray(Image.open('image.jpg'), dtype='uint8')
+whitened, background = whiten(image, kernel_size=50)
+Image.fromarray(whitened).save('whitened.jpg', 'jpeg')
+```
+
+CLI:
+```
+$ python whitening.py --help
+# whiten an image and save the whitened output
+$ python whitening.py input.jpg whitened.jpg
+# specify the kernel size
+$ python whitening.py input.jpg whitened.jpg -k 100
+# save also the background
+$ python whitening.py input.jpg whitened.jpg -b background.jpg -k 100
+```
+
+Select kernel size that's enough for not making artifacts while small enough
+to keep computation fast. A good starting point is 50 pixels.
+"""
+
+from __future__ import print_function
+import argparse
+import time
+
 import numpy as np
 from PIL import Image
 from skimage.filters import median
 from skimage.morphology import square
-
-def read_image_to_numpy(path):
-    return np.asarray(Image.open(path), dtype="uint8")
 
 def whiten(image, kernel_size):
     """
@@ -16,7 +49,8 @@ def whiten(image, kernel_size):
     the original image by the background, leaving the foreground.
 
     Input:
-    image - input image
+    `image` - input image
+    `kernel_size` - width of the median filter kernel
 
     All images are represented as a numpy array of shape (height, width,
     channels) and dtype uint8.
@@ -33,10 +67,37 @@ def whiten(image, kernel_size):
     whitened = (whitened * 255).astype(np.uint8)
     return whitened, background
 
+def timeit(method):
+    """
+    Decorator to measure run time of a method.
+    """
+    def timed(*args, **kw):
+        start = time.time()
+        result = method(*args, **kw)
+        end = time.time()
+        print('Run time: %2.2f sec' % (end - start))
+        return result
+
+    return timed
+
+@timeit
+def timed_whiten(image, kernel_size):
+    return whiten(image, kernel_size)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Whitens an image.')
+    parser.add_argument('image', metavar='INPUT', help='input image path')
+    parser.add_argument('whitened', metavar='WHITENED', help='whitened output image path')
+    parser.add_argument('-b', '--background', help='background output image path')
+    parser.add_argument('-k', '--kernel-size', type=int, default=50,
+        help='size of the 2D median filter kernel')
+
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    import os
-    data_dir = 'data/source_images/'
-    image = read_image_to_numpy(os.path.join(data_dir, 'IMG_3262_denoised.jpg'))
-    whitened, background = whiten(image, kernel_size=50)
-    Image.fromarray(whitened).save('IMG_3262_whitened.jpg', 'jpeg')
-    Image.fromarray(background).save('IMG_3262_background.jpg', 'jpeg')
+    args = parse_args()
+    image = np.asarray(Image.open(args.image), dtype='uint8')
+    whitened, background = timed_whiten(image, kernel_size=args.kernel_size)
+    Image.fromarray(whitened).save(args.whitened, 'jpeg')
+    if args.background is not None:
+        Image.fromarray(background).save(args.background, 'jpeg')
